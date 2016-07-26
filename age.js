bcf2982e5f9c7b4c7671e67c430a9052c0b424cc
Kate Walters <kawalters01@gmail.com>
2016-07-26 14:00:26 -0500

    Ruby Age Verifier done

---
+++ age.js
@@ -0,0 +1,7 @@
+function Verify (age) {
+  if age < 21
+  return "Too young";
+  else {
+    return "Proceed";
+  }
+}
---
+++ age.rb
@@ -0,0 +1,7 @@
+def verify(age)
+  if age < 21
+    print "Too young"
+  else
+    print "Proceed"
+  end
+end
