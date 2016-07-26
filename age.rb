0044b814ac3e23f6fc1dd01ec1c981b547a3915c
Kate Walters <kawalters01@gmail.com>
2016-07-26 14:22:15 -0500

    age.rb prints age responses

--- age.rb
+++ age.rb
@@ -1,7 +1,12 @@
 def verify(age)
-  if age < 21
-    print "Too young"
-  else
-    print "Proceed"
+  age.each do |a|
+    if a < 21
+      print "Too young"
+    else
+      print "Proceed"
+    end
   end
 end
+
+ages = [15, 20, 21, 100]
+verify(ages)
